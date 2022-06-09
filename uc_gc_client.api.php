77036605ee0c8e2af4bcbcdfbd65353db8a8f698
Rob Squires <rob@creativelocalsolutions.co.uk>
2022-06-09 10:48:38 +0100

    Initial commit of 7.x-2.x branch, with brand new Instant Payments functionality.

--- uc_gc_client.api.php
+++ uc_gc_client.api.php
@@ -16,7 +16,7 @@
  * @param array $mandate_details
  *   The alterable mandate details.
  */
-function hook_mandate_details_alter($order_id, array &$mandate_details) {
+function hook_mandate_details_alter(array &$mandate_details, $order_id) {
   $mandate_details['user']['first_name'] = 'Rob';
 }
 
@@ -77,4 +77,40 @@ function hook_payment_created($payment, $order_id, $op) {
 function hook_adjs_validate_alter(&$amount, &$price, $date, $order_id) {
   $amount = 0;
   $price = 0;
+}
+
+/**
+ * Alters the scheduled "next payment" date before saving to database.
+ *
+ * During cron runs, after a payment has been created for a "one-off" payments
+ * product, the scheduled "next payment" creation date is updated, according
+ * to the products scheduling rules. This hook can be used to override the
+ * default calculated "next payment" date.    
+ *
+ * @param int $next_payment
+ *   The calculated "next payment" date as a Unix timestamp.
+ * @param object $order
+ *   The Ubercart order for the subscription or payment.
+ */
+function hook_next_payment_alter(&$next_payment, $order) {
+  if ($order->order_id >= 100) {
+    $next_payment = strtotime('+1 month', $next_payment);
+  }
+}
+
+/**
+ * Alters the scheduled "next payment" date before saving to database.
+ *
+ * Upon completion of a checkout that generated an Instant Payment, the "next
+ * payment" date for "one-off" payments products is set to a future
+ * date that is determined by the scheduling rules for the product. Use this
+ * hook to override the calculated date.   
+ *
+ * @param int $next_payment
+ *   The calculated "next payment" date as a Unix timestamp.
+ * @param object $order
+ *   The Ubercart order for the subscription or payment.
+ */
+function hook_billing_request_next_payment_alter(&$next_payment, $order) {
+  $next_payment = strtotime('+2 weeks', REQUEST_TIME);
 }
